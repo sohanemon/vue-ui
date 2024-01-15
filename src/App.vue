75489e24064e9922a68a56e88021aa2b399af999
sohanemon <m.sohanemon@gmail.com>
2024-01-15 11:35:30 +0600

    feat(app): Add button component and update styles

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue';
+</script>
+
 <template>
-  <h1 class="text-3xl font-bold">Hello World</h1>
+  <div class="container flex flex-col items-center space-y-5 my-20">
+    <h1 class="text-3xl font-bold">
+      Welcome to Vue + Shadcn + Radix + Tailwind CSS + Typescript
+    </h1>
+    <Button>Get Started</Button>
+  </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(import.meta.dir, './src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
